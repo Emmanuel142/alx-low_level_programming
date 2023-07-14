55f7c7b9dc60d163a7a0014d6cb5663fb0b5dc6c
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-13 19:59:36 -0700

    edit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <string.h>
 /**
  * main - Entry point of the program
- * Description -this function print to the standard output without using 
- * printf and puts
- * Result - return 0;
+ *
+ * Description: This function prints to the screen without using printf and puts
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-
 	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	size_t len = strlen(message);
 	fwrite(message, sizeof(char), len, stdout);
