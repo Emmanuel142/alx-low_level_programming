2317eb4a921378bd77df9f49cd19dd1931d5c54a
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-13 18:11:57 -0700

    starting point of c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+/*this is the starting point of all c code*/
 int main(void)
 {
 	/*This code print an output to the screeen*/
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
