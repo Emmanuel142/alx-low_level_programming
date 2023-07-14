432f40f57cd5c04f87cc0338975949b18056df80
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-13 19:53:12 -0700

    should be done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - Entry point of the program
  * Description -this function print to the standard output without using 
  * printf and puts
  * Result - return 0;
  */
-#include <string.h>
+int main(void)
+{
 
-int main(void) {
-    const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    size_t len = strlen(message);
-
-    fwrite(message, sizeof(char), len, stdout);
-
-    return 0;
+	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t len = strlen(message);
+	fwrite(message, sizeof(char), len, stdout);
+	return (0);
 }
-
