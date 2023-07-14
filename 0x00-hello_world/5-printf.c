cb9d33187f7ed907d9f4a729d94756d404025519
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-13 18:54:20 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a word to the screen
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art, ");
