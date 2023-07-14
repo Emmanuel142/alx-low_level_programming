727c9b8c9baaf925595e40a0c531377ebed3633c
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-13 18:24:23 -0700

    dont even know what's wrong

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-/*let hope this
- * helps*/
 int main(void)
 {
-/*this returns a string*/
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
