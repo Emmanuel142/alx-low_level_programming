96d6dbbf27e7a62c0e70f1649e73348871e45412
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-14 12:39:29 -0700

    Last number test

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program generates a random number
+ * and write if it greater or lesser than 5
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int last;
+	       last = n % 10;
+
+	if (last > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	else if (last !=0 || last < 6)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+
+	return (0);
+}
