af5ad458a49c4cf74b924cdcdbb9d61536cf0405
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-14 16:01:28 -0700

    printing decimal digit with their ascii code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints  the decimal digit using ascii code 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int n;
+
+	i = 48;
+	n = 10;
+
+	for (i = 48; i <= 57; ++i)
+		putchar(i);
+	putchar(n);
+
+	return (0);
+}
