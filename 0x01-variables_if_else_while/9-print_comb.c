2d410ad13baa94e110b2acc710cfa69c50049d58
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-14 16:44:26 -0700

    now print with formatting, suprised right

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Staring point of the program
+ *
+ * Description: 
+ *
+ *Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int n;
+
+	i = 48;
+	j = 32;
+	k = 44;
+	n = 10;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(k);
+		putchar(j);
+	}
+	putchar(n);
+	return (0);
+}
