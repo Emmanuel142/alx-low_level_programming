e2eb715e3c1be7e944f9181dadcf7d1b3ec1a043
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-14 16:10:39 -0700

    dont know waht trailing white-space means at this junction

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point of the program
- * Description: Prints  the decimal digit using ascii code 
+ * Description: Prints  the decimal digit using ascii code
  * Return: Always 0 (Success)
  */
 int main(void)
