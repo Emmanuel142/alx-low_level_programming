73a8cf8dc371a519ad45ff86e4b9727e56323673
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-13 20:02:44 -0700

    when will this end

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <string.h>
 /**
  * main - Entry point of the program
- *
- * Description: This function prints to the screen without using printf and puts
+ * Description: This function prints to the screen
+ * without using printf and puts
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	size_t len = strlen(message);
 	fwrite(message, sizeof(char), len, stdout);
 	return (0);
