bd8ee8e0751b6e8cab6558da5a7a6d7e59b0f3aa
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-14 16:27:58 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Description: Prints  the decimal digit using ascii code
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int n;
+	int j;
+	
+	i = 48;
+	j = 97;
+	n = 10;
+
+	for (i = 48; i <= 57; ++i)
+		putchar(i);
+	for (j = 97; j <= 102; j++)
+		putchar(j);
+	putchar(n);
+	return (0);
+}
