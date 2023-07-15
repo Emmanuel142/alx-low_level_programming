b72963c1b91af374f2dc8512324ce8c3a304d214
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-14 17:49:21 -0700

    no replica either 11 or 12 21

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Staring point of the program
+ *
+ * Description: This function print formatted combination of decimal digit
+ * two digit should not be joined together and reverse of a letter should 
+ * not be printed
+ *Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int n;
+	int d;
+
+	i = 48;
+	j = 32;
+	k = 44;
+	n = 10;
+	d = i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (d = i; d <= 57; d++)
+		{
+			if (i == d)
+				continue;
+			putchar(i);
+			putchar(d);
+			if (i == 56 && d == 57)
+				break;
+			putchar(k);
+			putchar(j);
+		}
+	}
+	putchar(n);
+	return (0);
+}
