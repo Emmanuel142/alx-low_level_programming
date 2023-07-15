4ea1c6f858e6349f099296a4695b76d26b954b70
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-14 19:47:59 -0700

    mhhm

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,31 +6,22 @@
  * should not be printed.
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
     int num1, num2;
 
-    for (num1 = 0; num1 <= 9; num1++)
+    for (num1 = 0; num1 <= 8; num1++)
     {
         for (num2 = num1 + 1; num2 <= 9; num2++)
-
-    }
-
-    for (num1 = 10; num1 <= 98; num1++)
-    {
-        num2 = num1 % 10 * 10 + num1 / 10;
-
-        putchar(num1 / 10 + '0');
-        putchar(num1 % 10 + '0');
-        putchar(' ');
-        putchar(num2 / 10 + '0');
-        putchar(num2 % 10 + '0');
-
-        if (num1 != 98)
         {
-            putchar(',');
+            putchar(num1 / 10 + '0');
+            putchar(num1 % 10 + '0');
             putchar(' ');
+            putchar(num2 / 10 + '0');
+            putchar(num2 % 10 + '0');
+            putchar(num1 == 8 && num2 == 9 ? '\n' : ',');
+            putchar(num1 == 8 && num2 == 9 ? '\0' : ' ');
         }
     }
 
