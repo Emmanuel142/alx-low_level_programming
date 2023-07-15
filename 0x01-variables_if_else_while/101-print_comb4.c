d5ef9c167e601f0a5c39bb64d8789e1dac02cc4f
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-14 18:47:21 -0700

    much harder and trickier combination of three decimal digit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - Staring point of the program
+ *
+ * Description: This function print formatted combination of decimal digit
+ * three digit should not be joined together and reverse of a letter should
+ * not be printed
+ *Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int l;
+	int m;
+	int n;
+
+	a = 48;
+	b = 48;
+	c = 48;
+	l = 44;
+	m = 32;
+	n = 10;
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = a + 1; b <= 57; b++)
+		{
+		       	for (c = b + 1; c <= 57; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+			if (a == 55 && b == 56 && c == 57)
+				break;
+			putchar(l);
+			putchar(m);
+			}
+		}
+	}
+	putchar(n);
+	return (0);
+}
