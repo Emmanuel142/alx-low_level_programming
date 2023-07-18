9f373a13136359b5117fb18d813676a7d009ecb0
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-18 15:38:09 -0700

    Printing lowercase a-z with custom header file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - starting point of the code
+ * Description: This code write a - z
+ * Return; Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int n;
+
+	n = 10;
+
+	for (i = 97; i < 122; i++)
+		_putchar(i);
+	_putchar(n);
+
+	return (0);
+}
