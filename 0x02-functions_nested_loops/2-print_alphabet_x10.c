0b05e973d69cf091f193c0cde84f1c675d9471b5
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-18 16:44:30 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void);
+int main(void)
+{
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	
+	return (0);
+}
+void print_alphabet_x10(void)
+{
+	int i;
+	  int n;
+
+	n = 10;
+	i = 97;
+	
+	for (i = 97; i < 122; i++)
+		_putchar(i);
+	_putchar(n);
+}
