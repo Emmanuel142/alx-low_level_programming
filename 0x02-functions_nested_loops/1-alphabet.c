1bbd16994e6ce10fcf0b4cecdec29957bb183415
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-18 16:09:50 -0700

    jpp

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,24 +1,27 @@
 #include "main.h"
+
 void print_alphabet(void);
+
 /**
- * main - starting point of the code
- * Description: This code write a - z
- * Return; Always 0 (Success)
+ * main - Starting point of the code
+ *
+ * Description: This code writes the lowercase alphabets from 'a' to 'z'.
+ *
+ * Return: Always 0 (Success)
  */
-void print_alphabet(void)
-{
-	int i;
-	int n;
-
-	n = 10;
-	i = 97;
-
-	for (i = 97; i <= 122; i++)
-		_putchar(i);
-	_putchar(n);
-}
 int main(void)
 {
-	print_alphabet();
-	return (0);
+    print_alphabet();
+    return 0;
 }
+
+void print_alphabet(void)
+{
+    int i;
+
+    for (i = 'a'; i <= 'z'; i++)
+        _putchar(i);
+
+    _putchar('\n');
+}
+
