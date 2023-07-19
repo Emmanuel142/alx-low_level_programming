06967b9fe0b56c80aa07d4f719bb9963c52639df
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-18 17:35:28 -0700

    ll

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,23 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void);
-int main(void)
-{
-	print_alphabet_x10();
-	print_alphabet_x10();
-	print_alphabet_x10();
-	print_alphabet_x10();
-	print_alphabet_x10();
-	print_alphabet_x10();
-	print_alphabet_x10();
-	print_alphabet_x10();
-	print_alphabet_x10();
-	print_alphabet_x10();
-	
-	return (0);
-}
-void print_alphabet_x10(void)
+void print_alphabet_x10(void
 {
 	int i;
 	  int n;
