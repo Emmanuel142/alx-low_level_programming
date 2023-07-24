afb1b7b2e0eac776f73bc5966d4d613e29cdb936
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-24 21:44:36 +0100

    swapping intergers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - this function swap the values of intergers
+ * @a: The first input of the swap function
+ * @b: The second input of the swap function
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	int c = &a;
+	*a = b;
+	*b = c;
+}
