52df61dc85c33d417c16bc46e1f23f8a261bc088
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-25 00:40:44 +0100

    correct reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,9 +11,9 @@ void print_rev(char *s)
 	int j;
 
 	i = 0;
-	while (*s != '\0')
+	while (s[i] != '\0')
 		i++;
-	for (j = i; j > 0; j--)
-			_putchar(s[j]);
+	for (j = i - 1; j >= 0; j--)
+		_putchar(s[j]);
 	_putchar('\n');
 }
