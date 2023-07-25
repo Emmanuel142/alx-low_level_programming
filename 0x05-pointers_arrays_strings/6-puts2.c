77c040a6e1b923a3e2313b940b51d92e7a640e44
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-07-25 02:31:24 +0100

    puts file added

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,25 @@
 #include "main.h"
 
 /**
+ * puts2 - this function print every character or a string starting
+ * with first character follow by a new line
+ * @str: the string of character inputed
+ * Return: No return value
  */
+void puts2(char *str)
+{
+	int i;
+	int j;
 
+	i = 0;
+
+	while(str[i] != '/0')
+		i++;
+	for (j = 0; j <= i; i++)
+	{
+		if(j % 2 == 0)
+			_putchar(str[j]);
+	}
+
+	_putchar('\n');
+}
