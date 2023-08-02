5932b91d13fb4d5429cce1240b03e4d92daddbf2
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-08-02 14:55:09 -0700

    incomplete reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+void _print_rev_recursion(char *s);
+/**
+ * _print_rev_recursion - This function print to the standard
+ * output in reverse
+ * @s: The string of characters
+ * Return: return void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+
+	}
+}
