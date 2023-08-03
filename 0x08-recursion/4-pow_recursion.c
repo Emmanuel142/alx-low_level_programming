df1939df67f6fcbf0b0252b70e42d0af5e22b0a8
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-08-02 19:47:02 -0700

    negative square

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,6 +14,6 @@ int _pow_recursion(int x, int y)
 	if (y == 1)
 		return (x);
 	if (y < 0)
-		return (1 / _pow_recursion(x, -y));
+		return (1 / _pow_recursion(x, y - 1));
 	return (x * _pow_recursion(x, y - 1));
 }
