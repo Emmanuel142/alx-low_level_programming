31e183e126ee765a8273bd04cf0c7c5824685df1
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-08-02 19:06:49 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+int _pow_recursion(int x, int y);
+/**
+ * _pow_recursion - power of an interger
+ * @x: the base
+ * @y: the power
+ * Return: return the power if negative and positive and 1
+ * if the power is 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	if (y < 0)
+		return (1 / _pow_recursion(x, -y));
+	return (x * _pow_recursion(x, y - 1));
+}
