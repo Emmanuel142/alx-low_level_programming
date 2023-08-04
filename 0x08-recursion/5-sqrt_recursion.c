7fe9f73e2bc144775a648781d505b6fc08bb4e8f
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-08-03 17:46:34 -0700

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,33 @@
 #include "main.h"
+/**
+ * _sqrt_recursion - This function finds the;square root of an absolute number
+ * @n: The input
+ * Return: square root of n
+ */
 int _sqrt_recursion(int n);
 
 int findIntegerSquareRoot(int n, int start, int end)
 {
-	if (start > end)
-	{
-		return (end);
-	}
 	int mid = start + (end - start) / 2;
 	int square = mid * mid;
-	
-	if (square == n)
-	{
-		return mid;
-	}
+
+	if (start > end)
+		return (end);
+
+	else if (square == n)
+		return (mid);
+
 	else if (square < n)
-	{
-return findIntegerSquareRoot(n, mid + 1, end);
-} else {
-return findIntegerSquareRoot(n, start, mid - 1);
+		return (findIntegerSquareRoot(n, mid + 1, end));
+	else
+		return (findIntegerSquareRoot(n, start, mid - 1));
 }
-}
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	int start = 0;
+	int end = n;
 
-int _sqrt_recursion(int n) {
-if (n < 0) {
-return -1;
-}
-
-int start = 0;
-int end = n;
-
-return findIntegerSquareRoot(n, start, end);
+	return (findIntegerSquareRoot(n, start, end));
 }
