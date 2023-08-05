0e1b45432b010f08e5047280e395ca18e3346949
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-08-04 19:23:12 -0700

    argument counting

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * main - Entry of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
