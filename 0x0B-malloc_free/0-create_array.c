93002268763815051faf6b3d90d9c0755747e245
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-08-07 17:48:37 -0700

    comparism corrected

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,12 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return ('\0');
+		return (NULL);
 
 	ptr = malloc(sizeof(char) * size);
 
-	if (ptr == '\0')
-		return ('\0');
+	if (*ptr == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
