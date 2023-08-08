15b9c5e57219c7fd7b0d4514b5456df66c8f8f5f
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-08-07 17:44:18 -0700

    pointer to allocated memory for char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * create_array - Creates an array of chars and initializes it with a specific char
+ * create_array - Creates an array of chars and
+ * initializes it with a specific char
  * @size: The size of the array
  * @c: The char to initialize the array with
- * Return: Pointer to the newly created array, or NULL if size is 0 or allocation fails
+ * Return: Pointer to the newly created array,
+ * or NULL if size is 0 or allocation fails
  */
 char *create_array(unsigned int size, char c)
 {
