8e592d802f166ffebe644a8bdd6ea8daff21100f
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-08-16 18:12:32 -0700

    finally iterating properly

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,13 +6,15 @@
  * @array: array to execute func on
  * @size: is the size of the array
  * @action:  is a pointer to the function you need to use
- *
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	if (array && action)
+	{
+		size_t i;
 
-	for (i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		action(array[i]);
+	}
 }
