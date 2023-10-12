3f680974081236ee95629a29c6ba7b3f626f39e7
Emmanuel142 <emmanuel.ayoade.142@gmail.com>
2023-10-12 23:37:10 +0100

    Variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - this function sum up all integer passed into it
+ * @n: The num of argument
+ * Return: Return (0) if n is 0 and score if n is greater
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	int score = 0;
+
+	va_start(ap, n);
+
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+		score += va_arg(ap, int);
+	va_end(ap);
+	return (score);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - this function is used to print back
+ * the argument passed into it
+ * @separator: seperating symbols
+ * @n: number of parameter
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
