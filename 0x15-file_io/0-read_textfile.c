bbe6854741b9a8c95cb344b2de57150e13532554
“Emmanuel142” <Emmanuel.ayoade.142@gmail.com>
2023-10-30 19:55:13 -0700

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,32 @@
+#include "main.h"
+/**
+ * read_textfile - 
+ * filename: 
+ * letters:
+ * return:
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int o, r, w;
+	ssize_t o, r, w;
+	ssize_t buffer[letters];
 
-	if (letters < 1)
-		return ();
+	if (letters < 1 || filename == NULL)
+		return(0);
 	o = open(filename,O_RDWR);
-	if (o == NULL)
-		return (-1);
-	r = read(o, buffer, sizeof(buffer));
+	if (o == -1)
+		return (0);
+	r = read(o, buffer, letters);
+	if (r == -1)
+	{
+		close(o);
+		return (0);
+	}
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == -1)
+	{
+		close(r);
+		return (0);
+	}
+	close (o);
+	return (w);
 }
