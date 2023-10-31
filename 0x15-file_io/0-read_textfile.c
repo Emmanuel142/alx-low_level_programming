98a38cdd3d4157bb972a10776f538ad3b64eed33
“Emmanuel142” <Emmanuel.ayoade.142@gmail.com>
2023-10-30 20:34:48 -0700

    let use malloc instead of array

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,39 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * read_textfile - Read a text file  and print it tot the std output
- * @filename: Pointer to the name of the file
- * @letters: The letters is the number of letters it should read and print
- * return: 0 if unsucessful else the number of byte written
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
-	char buffer[1024];
+	char *buffer;
 
-	if (letters < 1 || filename == NULL)
+	if (filename == NULL)
 		return (0);
-	o = open(filename, O_RDWR);
-	if (o == -1)
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
+
+	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
-	if (r == -1)
-	{
-		close(o);
-		return (0);
-	}
 	w = write(STDOUT_FILENO, buffer, r);
-	if (w == -1)
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		close(o);
+		free(buffer);
 		return (0);
 	}
+
+	free(buffer);
 	close(o);
+
 	return (w);
 }
