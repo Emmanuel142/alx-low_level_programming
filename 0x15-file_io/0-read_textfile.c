6cdf57a11a4ef1c7fc0ef34c948a875457da5432
“Emmanuel142” <Emmanuel.ayoade.142@gmail.com>
2023-10-30 20:08:02 -0700

    betty style correction made

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * read_textfile - 
- * filename: 
- * letters:
- * return:
+ * read_textfile - Read a text file  and print it tot the std output
+ * @filename: Pointer to the name of the file
+ * @letters: The letters is the number of letters it should read and print
+ * return: 0 if unsucessful else the number of byte written
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -11,8 +11,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char buffer[letters];
 
 	if (letters < 1 || filename == NULL)
-		return(0);
-	o = open(filename,O_RDWR);
+		return (0);
+	o = open(filename, O_RDWR);
 	if (o == -1)
 		return (0);
 	r = read(o, buffer, letters);
@@ -27,6 +27,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(o);
 		return (0);
 	}
-	close (o);
+	close(o);
 	return (w);
 }
